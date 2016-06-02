d746d0039622ccd03a1d832052c06035b903cf88
Taylor Hurt <taylor.a.hurt@gmail.com>
2016-06-02 07:35:38 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,273 @@
+module.exports = {
+  trace, pipe, pipeP, map, intersection, difference, applyFunctions,
+  last, flip, curry, nth, adjust, toPairs, ifElse,
+  Maybe, Either, IO
+}
+
+
+// FUNCTIONS /////////////////////////////////////////////////////
+
+//:: a -> a
+const trace = (x) => {
+  console.log(x)
+  return x
+}
+
+//:: ((a, b, ... -> e), (e -> f), ..., (y -> z)) -> (a, b, ...) -> z
+const pipe = (...fns) => (...xs) => {
+  return fns
+    .slice(1)
+    .reduce((x, fn) => fn(x), fns[0](...xs))
+}
+const pipeP = (...fns) => (...xs) => {
+  return fns
+    .slice(1)
+    .reduce((xP, fn) => xP.then(fn), Promise.resolve(fns[0](...xs)))
+}
+
+//:: (a -> b) -> [a] -> [b]
+const map = (fn) => (f) => {
+  return f.map(fn)
+}
+
+//:: [a] -> [a] -> [a]
+const intersection = (xs) => (xs2) => {
+  return xs.filter(x => xs2.includes(x))
+}
+
+//:: [a] -> [a] -> [a]
+const difference = (xs) => (xs2) => {
+  return xs.filter(x => !xs2.includes(x))
+}
+
+//:: [(a, b, ...) -> n] -> [a, b, ...] -> [n]
+const applyFunctions = (fns) => (xs) => {
+  return fns.map(fn =>
+    xs.slice(1).reduce((partial, x) => partial(x), fn(xs[0])))
+}
+
+//:: [a] -> a
+const last = (xs) => {
+  return xs[xs.length - 1]
+}
+
+//:: (a -> b -> c) -> b -> a -> c
+const flip = (fn) => (b) => (a) => {
+  return fn(a)(b)
+}
+
+const curry = (fn) => {
+  var _args = []
+  const countArgs = (...xs) => {
+    _args = _args.concat(xs)
+    return (_args.length >= fn.length)
+      ? fn.apply(this, _args)
+      : countArgs
+  }
+  return countArgs
+}
+
+//:: Int -> [a] -> a
+const nth = (n) => (xs) => {
+  return xs[n]
+}
+
+//:: (a -> a) -> Number -> [a] -> [a]
+const adjust = (fn) => (i) => (list) => {
+  var copy = list.slice()
+  copy.splice(i, 1, fn(list[i]))
+  return copy
+}
+
+//:: Object -> Array
+const toPairs = (obj) => {
+  return Reflect.ownKeys(obj).map(key => [key, obj[key]])
+}
+
+//:: (a -> Bool) -> (a -> b) -> (a -> b) -> a -> b
+const ifElse = (predFn) => (whenTrueFn) => (whenFalseFn) => (a) =>{
+  return predFn(a)
+    ? whenTrueFn(a)
+    : whenFalseFn(a)
+}
+
+
+// this isn't in exports, it is used by IO.sequence //////////////
+const Generator = Object.freeze({
+  //:: (a -> b) -> (Generator ([a] -> b))
+  /* returns a generator which will apply
+     action to ea value sequentially in xs
+   */
+  seq(action) {
+    return function* applyAction(xs) {
+      for (var x of xs) {
+        yield action(x)
+      }
+    }
+  },
+  //:: Generator -> _
+  /* automatically steps generator every ~x ms
+     until the generator is exhausted
+   */
+  auto: (ms) => (gen) => {
+    if (!gen.next().done) {
+      setTimeout(() => Generator.auto(ms)(gen), ms)
+    }
+  }
+})
+
+
+// MONADS ///////////////////////////////////////////////////////
+
+// Maybe type
+const Maybe = (() => {
+  const newM = (type) => (value) => {
+    return Object.freeze(Object.create(type, { __value: { value: value }}))
+  }
+
+  const Nothing = Object.freeze({
+    map(_) {
+      return newM(Nothing)(null)
+    },
+    isNothing: true,
+    isJust: false
+  })
+
+  const Just = Object.freeze({
+    map(fn) {
+      return newM(Just)(fn(this.__value))
+    },
+    isNothing: false,
+    isJust: true
+  })
+
+  const Maybe = (x) => {
+    return (x == null)
+      ? newM(Nothing)(null)
+      : newM(Just)(x)
+  }
+
+  Maybe.isNothing = (M) => {
+    return Nothing.isPrototypeOf(M)
+  }
+
+  Maybe.isJust = (M) => {
+    return Just.isPrototypeOf(M)
+  }
+
+  return Object.freeze(Maybe)
+})()
+
+// Either type
+const Either = (() => {
+  const newE = (type) => (value) => {
+    return Object.freeze(Object.create(type, { __value: { value: value } }))
+  }
+
+  const Left = Object.freeze({
+    map(_) {
+      return this
+    },
+    bimap(fn) {
+      const me = this
+      return (_) => {
+        return newE(Left)(fn(me.__value))
+      }
+    },
+    isLeft: true,
+    isRight: false
+  })
+
+  const Right = Object.freeze({
+    map(fn) {
+      return newE(Right)(fn(this.__value))
+    },
+    bimap(_) {
+      const me = this
+      return (fn) => {
+        return me.map(fn)
+      }
+    },
+    isLeft: false,
+    isRight: true
+  })
+
+  const Either = Object.freeze({
+    Left(x) {
+      return newE(Left)(x)
+    },
+    Right(x) {
+      return newE(Right)(x)
+    },
+    isRight(E) {
+      return Right.isPrototypeOf(E)
+    },
+    isLeft(E) {
+      return Left.isPrototypeOf(E)
+    },
+    bimap: (leftFn) => (rightFn) => (E) => {
+      return E.bimap(leftFn)(rightFn)
+    }
+  })
+
+  return Either
+})()
+
+// IO type
+const IO = (() => {
+  const new_io = (fn) => {
+    return Object.freeze(Object.create(io, { __value: { value: fn }}))
+  }
+
+  const io = {
+    runIO(value) {
+      return this.__value(value)
+    },
+    map(fn) {
+      return new_io(() => fn(this.__value()))
+    },
+    join() {
+      return new_io(() => {
+        return this.runIO().runIO()
+      })
+    },
+    chain(io_returning_fn) {
+      return this.map(io_returning_fn).join()
+    },
+    ap(io_value) {
+      return io_value.map(this.__value)
+    }
+  }
+
+  const IO = (fn) => {
+    if (fn instanceof Function) {
+      return new_io(fn)
+    } else {
+      throw new TypeError(`IO constructor expected instance of Function`)
+    }
+  }
+
+  IO.of = (x) => {
+    return new_io(() => x)
+  }
+
+  IO.run = (io) => {
+    return io.runIO()
+  }
+
+  //:: (a -> b) -> a -> IO b
+  IO.wrap = (fn) => (_value) => {
+    return IO.of(_value).map(fn)
+  }
+
+  //:: [IO] -> IO _
+  IO.sequence = IO.wrap(
+    pipe(
+      Generator.seq(IO.run),
+      Generator.auto(0)
+    ))
+
+  return Object.freeze(IO)
+})()
+
+
